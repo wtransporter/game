f7d0aed54c0c53288d51622de4e5facf0f0b0768
Vladimir <vladapantic@hotmail.com>
2020-12-02 11:52:39 +0100

    Added button component

--- src/components/layouts/TheGame.vue
+++ src/components/layouts/TheGame.vue
@@ -5,19 +5,47 @@
 	<base-card>
 		<base-health>Player Health</base-health>
 	</base-card>
-	<base-card>
-		<button @click="playerAttack">Attack</button>
-	</base-card>
+
+	<div class="container">
+
+			<base-button @click="playerAttack">ATTACK</base-button>
+			<base-button @click="playerAttack">SPECIAL ATTACK</base-button>
+
+			<base-button @click="playerAttack">HEAL</base-button>
+			<base-button @click="playerAttack">SURRENDER</base-button>
+
+	</div>
+
 </template>
 
 <script>
 	import BaseCard from '../ui/BaseCard.vue';
 	import BaseHealth from '../ui/BaseHealth.vue';
+	import BaseButton from '../ui/BaseButton.vue';
 
 	export default {
 		components: {
 			BaseCard,
-			BaseHealth
+			BaseHealth,
+			BaseButton
 		},
+
+		methods: {
+			playerAttack() {
+
+			}
+		}
 	}
 </script>
+
+<style scoped>
+	.container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: center;
+		max-width: 640px;
+		margin: auto;
+	}
+</style>
---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+	<button>
+		<slot></slot>
+	</button>
+</template>
+
+<style scoped>
+	button {
+		font-family: inherit;
+		font-size: 16px;
+		background-color: green;
+		border: 1px solid green;
+		border-radius: 5px;
+		padding: 10px 32px;
+		margin: 20px;
+		color: white;
+		cursor: pointer;
+		outline-color: rgb(45, 153, 45);
+		min-width: 200px;
+	}
+	button:hover {
+		background-color: rgb(45, 153, 45);
+	}
+</style>
